65c146e855b1264510bf23605641dddf38a75b86
laufly <578939729@qq.com>
2017-11-08 17:42:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+关于webrtc js端sdk在安卓webview的测试:项目是相关代码:
+
+
+
+chrome的WebRTC开发文档，发现只有26版本的WebView才支持WebRTC。通过安卓5.0成功在系统内置浏览器中调用了摄像头与麦克风
+
+通过写入安卓权限，成功在5.0的系统中通过调用系统WebRTC内核实现视频语音聊天。但对系统版本要求较高，兼容性差
+
+
+参考文档:https://www.cnblogs.com/linl/p/4235001.html
+       http://blog.csdn.net/typename/article/details/40425275
+       http://blog.csdn.net/ren65432/article/details/53815832
+
+
+   注意:   1/ 如果想兼容4.0到5.0需要提取安卓5.0的WebView内核，并成功移植到项目中直接使用移植来的WebView，成功在安卓4.0-5.0所有平台上成功完成视频、语音聊天
+          2/ onPermissionRequest 回调中不能调用super的方法.
+
+
+   参考代码:
+
+  1.
+  webView.setWebChromeClient(new WebChromeClient() {
+             @Override
+             public void onPermissionRequest(final PermissionRequest request) {
+
+                 MainActivity.this.runOnUiThread(new Runnable(){
+                     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                     @Override
+                     public void run() {
+                         request.grant(request.getResources());
+                     }// run
+                 });// MainActivity
+
+             }// onPermissionRequest
+
+         });
+  2. 权限
+         <uses-permission android:name="android.permission.INTERNET"/>
+            <uses-permission android:name="android.permission.CAMERA" />
+            <uses-permission android:name="android.permission.CAPTURE_VIDEO_OUTPUT" />
---
+++ app/src/main/java/com/wilddog/userwebview/MainActivity.java
@@ -0,0 +1,206 @@
+package com.wilddog.userwebview;
+
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.GeolocationPermissions;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.PermissionRequest;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebStorage;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView view ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view = findViewById(R.id.wb_main);
+        view.loadUrl("https://www.wilddog.com/product/webrtc-demo/meeting?room=111&name=123&dimension=480p");
+        WebSettings webSettings = view.getSettings();
+        //设置WebView属性，能够执行Javascript脚本
+        webSettings.setJavaScriptEnabled(true);
+        //设置可以访问文件
+        webSettings.setAllowFileAccess(true);
+
+        //设置支持缩放
+        webSettings.setBuiltInZoomControls(true);
+        view.setWebViewClient(new WebViewClient(){
+          @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // TODO Auto-generated method stub
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        view.setWebChromeClient(new MyWebChromeClient());
+    }
+
+
+    class MyWebChromeClient extends WebChromeClient{
+        public MyWebChromeClient() {
+            super();
+        }
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            if(newProgress == 100){
+                Log.e("webchromeclient","100");
+            }
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+        }
+
+        @Override
+        public void onReceivedIcon(WebView view, Bitmap icon) {
+            super.onReceivedIcon(view, icon);
+        }
+
+        @Override
+        public void onReceivedTouchIconUrl(WebView view, String url, boolean precomposed) {
+            super.onReceivedTouchIconUrl(view, url, precomposed);
+        }
+
+        @Override
+        public void onShowCustomView(View view, CustomViewCallback callback) {
+            super.onShowCustomView(view, callback);
+        }
+
+        @Override
+        public void onShowCustomView(View view, int requestedOrientation, CustomViewCallback callback) {
+            super.onShowCustomView(view, requestedOrientation, callback);
+        }
+
+        @Override
+        public void onHideCustomView() {
+            super.onHideCustomView();
+        }
+
+        @Override
+        public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+            return super.onCreateWindow(view, isDialog, isUserGesture, resultMsg);
+        }
+
+        @Override
+        public void onRequestFocus(WebView view) {
+            super.onRequestFocus(view);
+        }
+
+        @Override
+        public void onCloseWindow(WebView window) {
+            super.onCloseWindow(window);
+        }
+
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            return super.onJsAlert(view, url, message, result);
+        }
+
+        @Override
+        public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+            return super.onJsConfirm(view, url, message, result);
+        }
+
+        @Override
+        public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+            return super.onJsPrompt(view, url, message, defaultValue, result);
+        }
+
+        @Override
+        public boolean onJsBeforeUnload(WebView view, String url, String message, JsResult result) {
+            return super.onJsBeforeUnload(view, url, message, result);
+        }
+
+        @Override
+        public void onExceededDatabaseQuota(String url, String databaseIdentifier, long quota, long estimatedDatabaseSize, long totalQuota, WebStorage.QuotaUpdater quotaUpdater) {
+            super.onExceededDatabaseQuota(url, databaseIdentifier, quota, estimatedDatabaseSize, totalQuota, quotaUpdater);
+        }
+
+        @Override
+        public void onReachedMaxAppCacheSize(long requiredStorage, long quota, WebStorage.QuotaUpdater quotaUpdater) {
+            super.onReachedMaxAppCacheSize(requiredStorage, quota, quotaUpdater);
+        }
+
+        @Override
+        public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+            super.onGeolocationPermissionsShowPrompt(origin, callback);
+        }
+
+        @Override
+        public void onGeolocationPermissionsHidePrompt() {
+            super.onGeolocationPermissionsHidePrompt();
+        }
+
+        @Override
+        public void onPermissionRequest(final PermissionRequest request) {
+            Log.e("webchromeclient","onPermissionRequest");
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                @Override
+                public void run() {
+                    request.grant(request.getResources());
+                }
+            });
+        }
+
+        @Override
+        public void onPermissionRequestCanceled(PermissionRequest request) {
+            super.onPermissionRequestCanceled(request);
+        }
+
+        @Override
+        public boolean onJsTimeout() {
+            return super.onJsTimeout();
+        }
+
+        @Override
+        public void onConsoleMessage(String message, int lineNumber, String sourceID) {
+            super.onConsoleMessage(message, lineNumber, sourceID);
+        }
+
+        @Override
+        public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+            return super.onConsoleMessage(consoleMessage);
+        }
+
+        @Override
+        public Bitmap getDefaultVideoPoster() {
+            return super.getDefaultVideoPoster();
+        }
+
+        @Override
+        public View getVideoLoadingProgressView() {
+            return super.getVideoLoadingProgressView();
+        }
+
+        @Override
+        public void getVisitedHistory(ValueCallback<String[]> callback) {
+            super.getVisitedHistory(callback);
+        }
+
+        @Override
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+            return super.onShowFileChooser(webView, filePathCallback, fileChooserParams);
+        }
+    }
+
+
+    // 结论:发现只有26以上版本的WebView才支持WebRTC
+}
